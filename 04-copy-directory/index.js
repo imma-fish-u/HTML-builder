fb9d11b2301ae7632416cb520b7b766341477bc1
imma-fish-u <63780616+imma-fish-u@users.noreply.github.com>
2021-11-09 13:51:52 +0300

    fix: minor bugs from first review

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,31 +4,30 @@ const path = require('path');
 const dirPath = path.join(__dirname, 'files'),
   dirPathCopy = path.join(__dirname, 'files-copy');
 
+let filePath, filePathCopy;
+
 fs.mkdir(dirPathCopy , {recursive: true}, (err) => {
   if (err) {
     return console.error(err);
   }
-  console.log('Directory created!');
-});
-
-let filePath, filePathCopy;
-
-fs.readdir(dirPath,  (err, files) => {
-  if (err) {
-    return console.error(err);
-  }
+  fs.readdir(dirPath,  (err, files) => {
+    if (err) {
+      return console.error(err);
+    }
+    
+    files.forEach(file => {
+      filePath = path.join(dirPath, file);
+      filePathCopy = path.join(dirPathCopy, file);
   
-  files.forEach(file => {
-    filePath = path.join(dirPath, file);
-    filePathCopy = path.join(dirPathCopy, file);
-
-    fs.copyFile(filePath, filePathCopy, (err) => {
-      if (err) {
-        return console.error(err);
-      }
-
-      console.log(`${filePath} was copied to ${filePathCopy}`);
+      fs.copyFile(filePath, filePathCopy, (err) => {
+        if (err) {
+          return console.error(err);
+        }
+  
+        console.log(`${filePath} was copied to ${filePathCopy}`);
+      });
     });
+  
   });
-
 });
+
