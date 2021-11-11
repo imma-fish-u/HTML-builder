0353879d6f678b7a0d088f06b382084d151f15c7
imma-fish-u <63780616+imma-fish-u@users.noreply.github.com>
2021-11-11 16:16:12 +0300

    fix: the folder should be removed before copying

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,28 +6,44 @@ const dirPath = path.join(__dirname, 'files'),
 
 let filePath, filePathCopy;
 
-fs.mkdir(dirPathCopy , {recursive: true}, (err) => {
-  if (err) {
-    return console.error(err);
-  }
-  fs.readdir(dirPath,  (err, files) => {
-    if (err) {
-      return console.error(err);
-    }
-    
-    files.forEach(file => {
-      filePath = path.join(dirPath, file);
-      filePathCopy = path.join(dirPathCopy, file);
-  
-      fs.copyFile(filePath, filePathCopy, (err) => {
+async function copyDirectory() {
+  return new Promise((resolve) => {
+    fs.mkdir(dirPathCopy, {recursive: true}, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+      
+      fs.readdir(dirPath,  (err, files) => {
         if (err) {
           return console.error(err);
         }
-  
-        console.log(`${filePath} was copied to ${filePathCopy}`);
+        
+        files.forEach(file => {
+          filePath = path.join(dirPath, file);
+          filePathCopy = path.join(dirPathCopy, file);
+
+          fs.copyFile(filePath, filePathCopy, (err) => {
+            if (err) {
+              return console.error(err);
+            }
+          
+            console.log(`${filePath} was copied to ${filePathCopy}`);
+            resolve();
+          });
+        }); 
       });
     });
-  
   });
+}
+
+
+fs.access(dirPathCopy, (err) =>{
+  if (err) {
+    copyDirectory();
+  } else {
+    fs.rm(dirPathCopy, { recursive: true, force: true }, () => {
+      copyDirectory();
+    });
+  }
 });
 
